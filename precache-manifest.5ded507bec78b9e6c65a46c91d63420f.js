29f6088f807804d300b0fcb4c2790950465172fe
Serhii Dykyi <vankyverd@gmail.com>
2020-06-30 13:26:18 +0300

    Updates

---
+++ precache-manifest.5ded507bec78b9e6c65a46c91d63420f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba6aad1edcd2b84ccb3c45b51c027be3",
+    "url": "/goit-react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "99465e1291cae07de56a",
+    "url": "/goit-react-hw-02-phonebook/static/css/2.4b502e0b.chunk.css"
+  },
+  {
+    "revision": "7dc9265eb58999376a14",
+    "url": "/goit-react-hw-02-phonebook/static/css/main.ae266ecd.chunk.css"
+  },
+  {
+    "revision": "99465e1291cae07de56a",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.4a1e5882.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.4a1e5882.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7dc9265eb58999376a14",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.fc0b59c9.chunk.js"
+  },
+  {
+    "revision": "65c8a2f1bf8ffcc01bde",
+    "url": "/goit-react-hw-02-phonebook/static/js/runtime-main.d8bf734b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/goit-react-hw-02-phonebook/precache-manifest.b6a493ac07e74efb541be474f9e90aba.js"
+  "/goit-react-hw-02-phonebook/precache-manifest.5ded507bec78b9e6c65a46c91d63420f.js"
 );
 
 self.addEventListener('message', (event) => {
